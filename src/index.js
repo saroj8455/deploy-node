0a38445f9f5eaaadabe23aedd30b8fca4f4eb04e
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-09-19 10:14:34 +0530

    hello world api test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("A app deploy into ubuntu server")
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log("A app deploy into ubuntu server")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
