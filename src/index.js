9e97e109df2d7f921041ea33737da40270108be2
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-09-20 09:57:39 +0530

    return json response

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import express from "express"
 const app = express()
 const port = 3000
 
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    console.log("A app deploy into ubuntu server")
+    res.json({
+        message:"OK , API check done"
+    })
 })
 
 app.listen(port, () => {
