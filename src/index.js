363a553494699f7aea9bc6a08b6b55929ba31fa8
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-09-20 13:21:21 +0530

    implement local auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import express from "express"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import httpStatus from "http-status"
+// validation of user input
+import Joi from "joi"
+import cors from "cors"
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -10,6 +16,47 @@ app.get('/', (req, res) => {
         message:"OK , API check done"
     })
 })
+
+/**
+ * @username,@password
+ * Auth api end point
+ */
+
+app.post("/auth",async (req,res)=>{
+    const loginSchema = Joi.object({
+        username:Joi.string().min(6).required().email(),
+        password:Joi.string().min(3).required()
+    })
+    const salt = await bcrypt.genSalt(10);
+    const hashPwd = await bcrypt.hash("ocemdev",salt)
+    console.log(hashPwd)
+    const {username,password} = req.body;
+    if (!username || !password) {
+        res.status(400).send({
+            message:"Enter username and password is mandatory field"
+        })
+        return;
+    }
+    // check password
+    const validPassword = await bcrypt.compare(password,hashPwd);
+    if(!validPassword) return res.status(400).send({
+        message:"Incorrect Password"
+    })
+    try {
+        const {error} =  loginSchema.validate(req.body);
+        if (error) {
+            return  res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+                error
+            });
+        }
+        return res.status(200).json({
+            username,password:hashPwd,validPassword
+        })
+    }catch (error) {
+        return res.status(500).send("Something wrong please try after sometime")
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
